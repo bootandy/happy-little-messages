21c0591f7c160f32ba0bf0546db1b0a19aff93a6
andy boot <bootandy@gmail.com>
2012-07-06 00:26:50 +0100

    bigger header. score system. store if you voted locally in a cookie

--- messages/models.py
+++ messages/models.py
@@ -4,7 +4,9 @@ from datetime import datetime
 class Message(models.Model):
     text = models.CharField(max_length=200)
     votes = models.IntegerField()
-    date_added = models.DateField(default=datetime.now)
+    date_added = models.DateTimeField(default=datetime.now)
+    score = models.BigIntegerField()
+    next_vote = models.IntegerField(default=60000)
 
     def __unicode__(self):
-        return self.text + ' : '+ str(self.votes) + ' : '+str(self.date_added)
+        return self.text + ' : '+ str(self.votes) + ' : '+str(self.date_added) + ' : ' + str(self.score) + ' : '+str(self.next_vote) + '\n'
--- messages/views.py
+++ messages/views.py
@@ -6,31 +6,31 @@ from messages.models import Message
 from django.http import Http404
 from django.template import RequestContext
 
+import time
+
 NUM_MSGS_TO_LOAD = 10
 
 def index(request):
     page_to_load = int(request.GET.get('pages', 0))
     scroll_top = int(request.GET.get('scroll_top', 0))
 
-    latest_msgs = Message.objects.all().order_by('-votes')[:(page_to_load+NUM_MSGS_TO_LOAD)]
+    latest_msgs = Message.objects.all().order_by('-score')[:(page_to_load+NUM_MSGS_TO_LOAD)]
+
+    votes = request.COOKIES.get('voted_for','').split(',')
+    votes = filter(None, votes)
+    vote_list = map(lambda a: int(a), votes);
+    
     return render_to_response('messages/index.html',  {
         'latest_msgs':latest_msgs, 
         'page_to_load':(page_to_load+NUM_MSGS_TO_LOAD),
-        'scroll_top':scroll_top
+        'scroll_top':scroll_top,
+        'votes': vote_list,
         }, 
         context_instance=RequestContext(request))
 
-def load(request):
-    latest_msgs = Message.objects.all().order_by('votes')[:10]
-
-
-# def detail(request, m_id):
-#     m = get_object_or_404(Message, pk=m_id) # get_list_or_404 also works :-)
-#     return render_to_response('messages/detail.html', {'message': m},  context_instance=RequestContext(request))
-
 def add(request):
     text = request.REQUEST['main_message_input']
-    m = Message(text=text, votes=0)
+    m = Message(text=text, votes=0, score=time.mktime(time.gmtime()))
     m.save()
     return HttpResponseRedirect(reverse('messages.views.index'))
 
@@ -38,16 +38,21 @@ def vote(request, m_id):
     p = get_object_or_404(Message, pk=m_id)
     try:
         p.votes += 1
+        p.score += p.next_vote
+        p.next_vote = int(p.next_vote * 2 / 3)
         p.save()
-    except (KeyError, Choice.DoesNotExist):
+
+    except (KeyError):
         # Redisplay the poll voting form.
         return render_to_response('messages/index.html', {
             'message': message,
             'error_message': "Error saving!",
         }, context_instance=RequestContext(request))
     else:
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('messages.views.index'))
+        res = HttpResponseRedirect(reverse('messages.views.index'))
+
+        votes = request.COOKIES.get('voted_for','') + ',' + str(p.id)
+        res.set_cookie('voted_for', votes)
+        print votes
+        return res
 
